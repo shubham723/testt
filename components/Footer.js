09863ca617f3bac0ac53f552b5f7458f262dca60
Shubham448 <73873707+Shubham448@users.noreply.github.com>
2022-03-09 23:13:36 +0530

    Latest Fixes

--- components/Footer.js
+++ components/Footer.js
@@ -2,47 +2,90 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center footer'>
-      <div className="footerContent">
-        <div className="footerHeadercontent">
-          <h1>
-            Maginite
-          </h1>
-        </div>
-        <div className="footerText">
-          <div className="t">
-            <h4>About</h4>
-          </div>
-          <div className="t">
-            <h4>Get in Touch</h4>
-          </div>
-          <div className="t">
-            <h4>Career</h4>
-          </div>
-          <div className="t">
-            <h4>Contact Us</h4>
-          </div>
-        </div>
-        <div className="finalfooterline">
-          <p>Call us at ****** &copy; by Maginite 2022</p>
-        </div>
-      </div>
+    <footer className="footer section gray-bg">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 mr-auto col-sm-6">
+            <div className="widget mb-5 mb-lg-0">
+              <div className="logo mb-4">
+                Magnitie
+                {/* <Image src={backgroundImage} alt="" className="img-fluid" /> */}
+              </div>
+              <p>Tempora dolorem voluptatum nam vero assumenda voluptate, facilis ad eos obcaecati tenetur veritatis eveniet distinctio possimus.</p>
 
-      {/* <h1 className='mt-3 pt-4'> Maginite </h1>
-      <div className='link mt-3'>
-        <p className='mr-3'>
-          About
-        </p>
-        <p> 
-          Get in Touch
-        </p>
-        <p> 
-          Career
-        </p>
+              {/* <ul className="list-inline footer-socials mt-4">
+                        <li className="list-inline-item"><a href="https://www.facebook.com/themefisher"><i className="icofont-facebook"></i></a></li>
+                        <li className="list-inline-item"><a href="https://twitter.com/themefisher"><i className="icofont-twitter"></i></a></li>
+                        <li className="list-inline-item"><a href="https://www.pinterest.com/themefisher/"><i className="icofont-linkedin"></i></a></li>
+                    </ul> */}
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="widget mb-5 mb-lg-0">
+              <h4 className="text-capitalize mb-3">Support</h4>
+              <div className="divider mb-4"></div>
+
+              <ul className="list-unstyled footer-menu lh-35">
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Company Support </a></li>
+                <li><a href="#">FAQuestions</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="widget widget-contact mb-5 mb-lg-0">
+              <h4 className="text-capitalize mb-3">Get in Touch</h4>
+              <div className="divider mb-4"></div>
+
+              <div className="footer-contact-block mb-4">
+                <div className="icon d-flex align-items-center">
+                  <i className="icofont-email mr-3"></i>
+                  <span className="h6 mb-0">Support Available for 24/7</span>
+                </div>
+                <h4 className="mt-2"><a href="tel:+23-345-67890">Support@email.com</a></h4>
+              </div>
+
+              <div className="footer-contact-block">
+                <div className="icon d-flex align-items-center">
+                  <i className="icofont-support mr-3"></i>
+                  <span className="h6 mb-0">Mon to Fri : 08:30 - 18:00</span>
+                </div>
+                <h4 className="mt-2"><a href="tel:+23-345-67890">+23-456-6588</a></h4>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-btm py-4 mt-5">
+          <div className="row align-items-center justify-content-between">
+            <div className="col-lg-6">
+              <div className="copyright">
+                &copy; Copyright Reserved to <span className="text-color">Magnitie</span>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="subscribe-form text-lg-right mt-5 mt-lg-0">
+                <form action="#" className="subscribe">
+                  <input type="text" className="form-control" placeholder="Your Email address" />
+                  <a href="#" className="btn btn-main-2 btn-round-full subscribe-btn">Subscribe</a>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4">
+              <a className="backtop js-scroll-trigger" href="#top">
+                <i className="icofont-long-arrow-up"></i>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      <section className='mt-3'>Powered By Blogger</section>
-      <section>Call us at ****** &copy; by Maginite 2022</section> */}
-    </div>
+    </footer>
   )
 }
 
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import Image from 'next/image';
 import banner from '../assets/Images/banner.jpg';
 import blog from '../assets/Images/blog.jpg';
@@ -6,17 +6,54 @@ import blog from '../assets/Images/blog.jpg';
 const Header = ({ head }) => {
     return (
         <div style={{ backgroundColor: 'black', color: 'white' }}>
-            <Container fluid>
-                <Row>
-                    <Col className="headersection" md={6} xs={12}>
-                        <h1> {head} </h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                    </Col>
-                    <Col md={6} xs={12}>
-                        <Image src={blog} alt="header" />
-                    </Col>
-                </Row>
-            </Container>
+            <header>
+                {/* <div className="header-top-bar">
+                    <div className="container">
+                        <div className="row align-items-center">
+                            <div className="col-lg-6">
+                                <ul className="top-bar-info list-inline-item pl-0 mb-0">
+                                    <li className="list-inline-item"><a href="mailto:support@gmail.com"><i className="icofont-support-faq mr-2"></i>support@magnite.com</a></li>
+                                    <li className="list-inline-item"><i className="icofont-location-pin mr-2"></i>Dummy Address </li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="text-lg-right top-right-bar mt-2 mt-lg-0">
+                                    <a href="tel:+23-345-67890" >
+                                        <span>Call Now : </span>
+                                        <span className="h4">*******</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#">Magnitie</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                            </Nav>
+                            <Nav className="d-flex">
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="blog-sidebar.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog <i className="icofont-thin-down"></i></a>
+                                    <ul className="dropdown-menu" aria-labelledby="dropdown05">
+                                        <li><a className="dropdown-item" href="blog-sidebar.html">Blog with Sidebar</a></li>
+
+                                        <li><a className="dropdown-item" href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li>
+                                <Nav.Link href="">Contact</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </header>
         </div>
     )
 };
