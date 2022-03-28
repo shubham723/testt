28406ec19c48bd65122f8c59d5425eeedd6ea609
Shubham448 <you@example.com>
2022-03-28 18:43:26 +0530

    latest changes

--- pages/index.js
+++ pages/index.js
@@ -73,8 +73,8 @@ const Home = () => {
           />
         </div> :
           <>
-            <Banner settings={settings} />
-            {/* <section className="section about">
+            {/* <Banner settings={settings} /> */}
+            <section className="section about">
               <div className="container">
                 <div className="row align-items-center">
                   <div className="col-lg-5 col-sm-6">
@@ -91,9 +91,9 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-            </section> */}
+            </section>
 
-            {/* <section className="section service">
+            <section className="section service">
               <div className="container">
                 <div className="row justify-content-center">
                   <div className="col-lg-7 text-center">
@@ -131,7 +131,7 @@ const Home = () => {
                   }
                 </div>
               </div>
-            </section> */}
+            </section>
           </>
       }
     </div>
