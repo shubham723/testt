671798a892dcb6f7be36489362960b65041bf801
Shubham448 <you@example.com>
2022-03-28 18:26:32 +0530

    latest changes

--- pages/_app.js
+++ pages/_app.js
@@ -9,27 +9,27 @@ import ToggleNotification from '../components/Reusable Components/ToogleNotifica
 import axios from 'axios';
 
 function MyApp({ Component, pageProps }) {
-  // const [settings, setSettings] = useState({});
+  const [settings, setSettings] = useState({});
 
-  // useEffect(() => {
-  //   getSettings();
-  // }, []);
+  useEffect(() => {
+    getSettings();
+  }, []);
 
-  // const getSettings = async () => {
-  //   try {
-  //     let response = await axios.get('http://3.88.73.172:3001/v1/settings');
-  //     setSettings(response.data.data);
-  //   } catch (error) {
-  //     ToggleNotification("Error", error?.response?.data?.message);
-  //   }
-  // };
+  const getSettings = async () => {
+    try {
+      let response = await axios.get('http://3.88.73.172:3001/v1/settings');
+      setSettings(response.data.data);
+    } catch (error) {
+      ToggleNotification("Error", error?.response?.data?.message);
+    }
+  };
 
   return (
     <>
       <NotificationContainer />
-      {/* <Header settings={settings} /> */}
+      <Header settings={settings} />
       <Component {...pageProps} />
-      {/* <Footer settings={settings} /> */}
+      <Footer settings={settings} />
     </>
   )
 };
