798edc5d482bf278f8d41ce676443264fc013553
Shubham448 <you@example.com>
2022-03-28 19:01:30 +0530

    latest changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const withTM = require('next-transpile-modules')(['@babylonjs/core']); // As per comment.
-const withPlugins = require('next-compose-plugins');
-
-const nextConfig = {
-    target: 'serverless',
-    webpack: function (config) {
-        /// below is not required for the problem described. Just for reference.(es6)
-        config.module.rules.push({test: /\.yml$/, use: 'raw-loader'})
-        return config
-    }
+// next.config.js
+module.exports = {
+    images: {
+      domains: ['3.88.73.172'],
+    },
 }
-
-module.exports = withPlugins([withTM], nextConfig);
